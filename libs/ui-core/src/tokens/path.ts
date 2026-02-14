9e2c85efeee206fa777c847aaa9a23aa0c816407
Junhyuk Park <junhyeg08@gmail.com>
2026-02-14 10:04:08 +0900

    refactor(ui-core): 타입 정의 및 개선
    - LeafDotPath 타입을 재작성하여 중첩된 객체 경로 추출 로직 간소화
    - 코드 가독성과 유지보수성 향상을 위해 타입스크립트 타입 구조 개선

--- libs/ui-core/src/tokens/path.ts
+++ libs/ui-core/src/tokens/path.ts
@@ -1,19 +1,18 @@
-type AnyFn = (...args: unknown[]) => unknown;
+type Primitive = string | number | boolean | bigint | symbol | null | undefined;
 
-type IsPlainObject<T> = T extends object
-  ? T extends AnyFn
-    ? false
-    : T extends readonly unknown[]
-      ? false
-      : true
-  : false;
+type TokenLeaf = { value: unknown; type?: unknown };
 
-export type LeafDotPath<T> =
-  IsPlainObject<T> extends true
-    ? {
-        [K in keyof T & string]: IsPlainObject<T[K]> extends true ? `${K}.${LeafDotPath<T[K]>}` : K;
-      }[keyof T & string]
-    : never;
+export type LeafDotPath<T> = T extends Primitive
+  ? never
+  : {
+      [K in Extract<keyof T, string>]: T[K] extends TokenLeaf
+        ? K
+        : T[K] extends Primitive
+          ? K
+          : T[K] extends Record<string, unknown>
+            ? `${K}.${LeafDotPath<T[K]>}`
+            : K;
+    }[Extract<keyof T, string>];
 
 export type PathValue<T, P extends string> = P extends `${infer K}.${infer Rest}`
   ? K extends keyof T
